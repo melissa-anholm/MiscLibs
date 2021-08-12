a6f573e8a0e42cb132c2cf63f662ef920451f6ef
anholm <>
2021-08-12 11:08:18 -0700

    Ignoring some of the output files.

--- ReTuple/multituple_the_data.sh
+++ ReTuple/multituple_the_data.sh
@@ -1,48 +1,48 @@
 #            runno g4 ind sig 
 
-## The default set:
-#printf '%s%s\n' "The Default Set. SNR=0.25@i=0, and sigma_agreement = 3.0."; 
-#printf '%s%s\n' "Starting on the Set B Electrons: "; 
-#./multituple 428 0 0 3.0
-#./multituple 429 0 0 3.0
-#./multituple 430 0 0 3.0
-#./multituple 431 0 0 3.0
-#./multituple 432 0 0 3.0
-#./multituple 433 0 0 3.0
-#./multituple 434 0 0 3.0
-#./multituple 435 0 0 3.0
-#./multituple 436 0 0 3.0  #
-#./multituple 437 0 0 3.0
-#./multituple 440 0 0 3.0
-#./multituple 441 0 0 3.0
-#./multituple 442 0 0 3.0
-#./multituple 443 0 0 3.0
-#./multituple 444 0 0 3.0
-#./multituple 445 0 0 3.0
-#printf '%s%s\n' "Starting on the Set C Electrons: "; 
-#./multituple 476 0 0 3.0
-#./multituple 477 0 0 3.0
-#printf '%s%s\n' "Starting on the Set D Electrons: "; 
-#printf '%s%s\n' "(Runs 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 502, 503, 504, 505, 510, 513)"; 
-#./multituple 478 0 0 3.0
-#./multituple 479 0 0 3.0
-#./multituple 480 0 0 3.0
-#./multituple 481 0 0 3.0
-#./multituple 482 0 0 3.0
-#./multituple 483 0 0 3.0
-#./multituple 484 0 0 3.0
-#./multituple 485 0 0 3.0
-#./multituple 486 0 0 3.0
-#./multituple 487 0 0 3.0  # short run.  but sometimes I don't even.
-#./multituple 488 0 0 3.0  # short run.  but sometimes I don't even.
-#./multituple 489 0 0 3.0  # short run.  but sometimes I don't even.
-#./multituple 502 0 0 3.0
-#./multituple 503 0 0 3.0
-#./multituple 504 0 0 3.0
-#./multituple 505 0 0 3.0
-#./multituple 510 0 0 3.0
-#./multituple 513 0 0 3.0
-#printf '%s%s\n' "Done with multi-set 1!"; 
+# The default set:
+printf '%s%s\n' "The Default Set. SNR=0.25@i=0, and sigma_agreement = 3.0."; 
+printf '%s%s\n' "Starting on the Set B Electrons: "; 
+./multituple 428 0 0 3.0
+./multituple 429 0 0 3.0
+./multituple 430 0 0 3.0
+./multituple 431 0 0 3.0
+./multituple 432 0 0 3.0
+./multituple 433 0 0 3.0
+./multituple 434 0 0 3.0
+./multituple 435 0 0 3.0
+./multituple 436 0 0 3.0  #
+./multituple 437 0 0 3.0
+./multituple 440 0 0 3.0
+./multituple 441 0 0 3.0
+./multituple 442 0 0 3.0
+./multituple 443 0 0 3.0
+./multituple 444 0 0 3.0
+./multituple 445 0 0 3.0
+printf '%s%s\n' "Starting on the Set C Electrons: "; 
+./multituple 476 0 0 3.0
+./multituple 477 0 0 3.0
+printf '%s%s\n' "Starting on the Set D Electrons: "; 
+printf '%s%s\n' "(Runs 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 502, 503, 504, 505, 510, 513)"; 
+./multituple 478 0 0 3.0
+./multituple 479 0 0 3.0
+./multituple 480 0 0 3.0
+./multituple 481 0 0 3.0
+./multituple 482 0 0 3.0
+./multituple 483 0 0 3.0
+./multituple 484 0 0 3.0
+./multituple 485 0 0 3.0
+./multituple 486 0 0 3.0
+./multituple 487 0 0 3.0  # short run.  but sometimes I don't even.
+./multituple 488 0 0 3.0  # short run.  but sometimes I don't even.
+./multituple 489 0 0 3.0  # short run.  but sometimes I don't even.
+./multituple 502 0 0 3.0
+./multituple 503 0 0 3.0
+./multituple 504 0 0 3.0
+./multituple 505 0 0 3.0
+./multituple 510 0 0 3.0
+./multituple 513 0 0 3.0
+printf '%s%s\n' "Done with multi-set 1!"; 
 
 #   ###   #
 # Adjust SNR index. SNR=0.25@i=0, SNR=0.50@i=1, SNR=0.75@i=2, SNR=1.0@i=3, SNR=2.0@i=4.
--- ReTuple/multituple_the_g4.sh
+++ ReTuple/multituple_the_g4.sh
@@ -1,6 +1,60 @@
 printf '%s%s\n' "Here we go!"; 
 #            runno g4 ind sig 
 
+# Adjust SNR index. SNR=0.25@i=0, SNR=0.50@i=1, SNR=0.75@i=2, SNR=1.0@i=3, SNR=2.0@i=4.
+#printf '%s%s\n' "Adjust SNR index. SNR=0.5@i=2."; 
+#./multituple 10005 1 2 3.0
+#./multituple 10006 1 2 3.0
+#./multituple 10007 1 2 3.0
+#./multituple 10008 1 2 3.0
+#./multituple 10065 1 2 3.0
+#./multituple 10066 1 2 3.0
+#./multituple 10001 1 2 3.0
+#./multituple 10002 1 2 3.0
+#./multituple 10013 1 2 3.0
+#./multituple 10014 1 2 3.0
+#./multituple 10009 1 2 3.0
+#./multituple 10010 1 2 3.0
+#./multituple 10011 1 2 3.0
+#./multituple 10012 1 2 3.0
+#printf '%s%s\n' "Done with multi-set 6!"; 
+
+
+printf '%s%s\n' "Adjust the sigma agreement.  Up slightly.  "; 
+./multituple 10005 1 0 3.5
+./multituple 10006 1 0 3.5
+./multituple 10007 1 0 3.5
+./multituple 10008 1 0 3.5
+./multituple 10065 1 0 3.5
+./multituple 10066 1 0 3.5
+./multituple 10001 1 0 3.5
+./multituple 10002 1 0 3.5
+./multituple 10013 1 0 3.5
+./multituple 10014 1 0 3.5
+./multituple 10009 1 0 3.5
+./multituple 10010 1 0 3.5
+./multituple 10011 1 0 3.5
+./multituple 10012 1 0 3.5
+printf '%s%s\n' "Done with multi-set 7!"; 
+
+printf '%s%s\n' "Adjust the sigma agreement.  Now Down slightly.  "; 
+./multituple 10005 1 0 2.5
+./multituple 10006 1 0 2.5
+./multituple 10007 1 0 2.5
+./multituple 10008 1 0 2.5
+./multituple 10065 1 0 2.5
+./multituple 10066 1 0 2.5
+./multituple 10001 1 0 2.5
+./multituple 10002 1 0 2.5
+./multituple 10013 1 0 2.5
+./multituple 10014 1 0 2.5
+./multituple 10009 1 0 2.5
+./multituple 10010 1 0 2.5
+./multituple 10011 1 0 2.5
+./multituple 10012 1 0 2.5
+printf '%s%s\n' "Done with multi-set 8!"; 
+
+
 ## The default set:
 #printf '%s%s\n' "The Default Set. SNR=0.25@i=0, and sigma_agreement = 3.0."; 
 #./multituple 10005 1 0 3.0
@@ -20,78 +74,78 @@ printf '%s%s\n' "Here we go!";
 #printf '%s%s\n' "Done with multi-set 1!"; 
 
 #   ###   #
-# Adjust SNR index. SNR=0.25@i=0, SNR=0.50@i=1, SNR=0.75@i=2, SNR=1.0@i=3, SNR=2.0@i=4.
-printf '%s%s\n' "Adjust SNR index. SNR=0.5@i=1."; 
-./multituple 10005 1 1 3.0
-./multituple 10006 1 1 3.0
-./multituple 10007 1 1 3.0
-./multituple 10008 1 1 3.0
-./multituple 10065 1 1 3.0
-./multituple 10066 1 1 3.0
-./multituple 10001 1 1 3.0
-./multituple 10002 1 1 3.0
-./multituple 10013 1 1 3.0
-./multituple 10014 1 1 3.0
-./multituple 10009 1 1 3.0
-./multituple 10010 1 1 3.0
-./multituple 10011 1 1 3.0
-./multituple 10012 1 1 3.0
-printf '%s%s\n' "Done with multi-set 2!"; 
+## Adjust SNR index. SNR=0.25@i=0, SNR=0.50@i=1, SNR=0.75@i=2, SNR=1.0@i=3, SNR=2.0@i=4.
+#printf '%s%s\n' "Adjust SNR index. SNR=0.5@i=1."; 
+#./multituple 10005 1 1 3.0
+#./multituple 10006 1 1 3.0
+#./multituple 10007 1 1 3.0
+#./multituple 10008 1 1 3.0
+#./multituple 10065 1 1 3.0
+#./multituple 10066 1 1 3.0
+#./multituple 10001 1 1 3.0
+#./multituple 10002 1 1 3.0
+#./multituple 10013 1 1 3.0
+#./multituple 10014 1 1 3.0
+#./multituple 10009 1 1 3.0
+#./multituple 10010 1 1 3.0
+#./multituple 10011 1 1 3.0
+#./multituple 10012 1 1 3.0
+#printf '%s%s\n' "Done with multi-set 2!"; 
 
-# Adjust SNR index again. SNR=0.25@i=0, SNR=0.50@i=1, SNR=0.75@i=2, SNR=1.0@i=3, SNR=2.0@i=4.
-printf '%s%s\n' "Adjust SNR index. SNR=1.0@i=3."; 
-./multituple 10005 1 3 3.0
-./multituple 10006 1 3 3.0
-./multituple 10007 1 3 3.0
-./multituple 10008 1 3 3.0
-./multituple 10065 1 3 3.0
-./multituple 10066 1 3 3.0
-./multituple 10001 1 3 3.0
-./multituple 10002 1 3 3.0
-./multituple 10013 1 3 3.0
-./multituple 10014 1 3 3.0
-./multituple 10009 1 3 3.0
-./multituple 10010 1 3 3.0
-./multituple 10011 1 3 3.0
-./multituple 10012 1 3 3.0
-printf '%s%s\n' "Done with multi-set 3!"; 
-
-
-# Adjust the sigma agreement.  Down first:
-printf '%s%s\n' "Adjust the sigma agreement.  Down first.  "; 
-./multituple 10005 1 0 2.0
-./multituple 10006 1 0 2.0
-./multituple 10007 1 0 2.0
-./multituple 10008 1 0 2.0
-./multituple 10065 1 0 2.0
-./multituple 10066 1 0 2.0
-./multituple 10001 1 0 2.0
-./multituple 10002 1 0 2.0
-./multituple 10013 1 0 2.0
-./multituple 10014 1 0 2.0
-./multituple 10009 1 0 2.0
-./multituple 10010 1 0 2.0
-./multituple 10011 1 0 2.0
-./multituple 10012 1 0 2.0
-printf '%s%s\n' "Done with multi-set 4!"; 
+## Adjust SNR index again. SNR=0.25@i=0, SNR=0.50@i=1, SNR=0.75@i=2, SNR=1.0@i=3, SNR=2.0@i=4.
+#printf '%s%s\n' "Adjust SNR index. SNR=1.0@i=3."; 
+#./multituple 10005 1 3 3.0
+#./multituple 10006 1 3 3.0
+#./multituple 10007 1 3 3.0
+#./multituple 10008 1 3 3.0
+#./multituple 10065 1 3 3.0
+#./multituple 10066 1 3 3.0
+#./multituple 10001 1 3 3.0
+#./multituple 10002 1 3 3.0
+#./multituple 10013 1 3 3.0
+#./multituple 10014 1 3 3.0
+#./multituple 10009 1 3 3.0
+#./multituple 10010 1 3 3.0
+#./multituple 10011 1 3 3.0
+#./multituple 10012 1 3 3.0
+#printf '%s%s\n' "Done with multi-set 3!"; 
 
 
-# Adjust the sigma agreement.  Up next:
-printf '%s%s\n' "Adjust the sigma agreement.  Up next.  "; 
-./multituple 10005 1 0 4.0
-./multituple 10006 1 0 4.0
-./multituple 10007 1 0 4.0
-./multituple 10008 1 0 4.0
-./multituple 10065 1 0 4.0
-./multituple 10066 1 0 4.0
-./multituple 10001 1 0 4.0
-./multituple 10002 1 0 4.0
-./multituple 10013 1 0 4.0
-./multituple 10014 1 0 4.0
-./multituple 10009 1 0 4.0
-./multituple 10010 1 0 4.0
-./multituple 10011 1 0 4.0
-./multituple 10012 1 0 4.0
-printf '%s%s\n' "Done with multi-set 5!"; 
+## Adjust the sigma agreement.  Down first:
+#printf '%s%s\n' "Adjust the sigma agreement.  Down first.  "; 
+#./multituple 10005 1 0 2.0
+#./multituple 10006 1 0 2.0
+#./multituple 10007 1 0 2.0
+#./multituple 10008 1 0 2.0
+#./multituple 10065 1 0 2.0
+#./multituple 10066 1 0 2.0
+#./multituple 10001 1 0 2.0
+#./multituple 10002 1 0 2.0
+#./multituple 10013 1 0 2.0
+#./multituple 10014 1 0 2.0
+#./multituple 10009 1 0 2.0
+#./multituple 10010 1 0 2.0
+#./multituple 10011 1 0 2.0
+#./multituple 10012 1 0 2.0
+#printf '%s%s\n' "Done with multi-set 4!"; 
+#
+#
+## Adjust the sigma agreement.  Up next:
+#printf '%s%s\n' "Adjust the sigma agreement.  Up next.  "; 
+#./multituple 10005 1 0 4.0
+#./multituple 10006 1 0 4.0
+#./multituple 10007 1 0 4.0
+#./multituple 10008 1 0 4.0
+#./multituple 10065 1 0 4.0
+#./multituple 10066 1 0 4.0
+#./multituple 10001 1 0 4.0
+#./multituple 10002 1 0 4.0
+#./multituple 10013 1 0 4.0
+#./multituple 10014 1 0 4.0
+#./multituple 10009 1 0 4.0
+#./multituple 10010 1 0 4.0
+#./multituple 10011 1 0 4.0
+#./multituple 10012 1 0 4.0
+#printf '%s%s\n' "Done with multi-set 5!"; 
 
 
